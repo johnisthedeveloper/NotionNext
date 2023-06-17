486db3cd344ae12815578afc577e7f139a4660ac
JohnDeveloper <johnisthedeveloper@gmail.com>
2023-06-17 14:25:58 -0400

    Update LayoutIndex.js

--- themes/matery/LayoutIndex.js
+++ themes/matery/LayoutIndex.js
@@ -8,7 +8,7 @@ import React from 'react'
 import Announcement from './components/Announcement'
 
 export const LayoutIndex = (props) => {
-  return <LayoutBase {...props} />} headerSlot={CONFIG_MATERY.HOME_BANNER_ENABLE && <Header {...props} />}>
+  return <LayoutBase {...props} containerSlot={ <Announcement {...props}/>} headerSlot={CONFIG_MATERY.HOME_BANNER_ENABLE && <Header {...props} />}>
     {BLOG.POST_LIST_STYLE === 'page' ? <BlogPostListPage {...props} /> : <BlogPostListScroll {...props} />}
   </LayoutBase>
 }
